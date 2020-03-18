ead4bce449438cc97091547b41e7a9a94329063d
Ronald Baldwin <robaldwin9@gmail.com>
2020-03-17 20:25:01 -0400

    Made Arguments optional  - Added  ability to retain ip via dhcp  - Added no args functionality to print current ip info  - Added lnic command to list all NICs information  - Updated name of app in argsparser  - Added function to see if IP is reachable(taken)

--- src/pyipchg.py
+++ src/pyipchg.py
@@ -1,41 +1,80 @@
 import wmi 
 import argparse
+import sys
+import os
 
 # Obtain network adaptors configurations
 nic_configs = wmi.WMI().Win32_NetworkAdapterConfiguration(IPEnabled=True)
 
-def ipChange(args):
-	ip =  args.IP
-	sub = args.SUBNET
-	gateway = args.GATWAY
+# Current values for ip configuration, TODO make nic_configs index an argument!
+currentSubnet = nic_configs[0].IPSubnet[0]
+currentGateway = nic_configs[0].DefaultIPGateway[0]
+currentIP = nic_configs[0].IPAddress[0]
 
+# Change ip based on users args
+def ipChange(args):
+	ip =  args.ip
+
+	# Set Gateway
+	gateway = currentGateway
+	if args.gat is not None:
+		gateway = args.gat
+	
+	# Set Subnet
+	sub = currentSubnet
+	if args.sub is not None:
+		sub = args.sub
+	
+	# Set IP
 	nic = nic_configs[0]
 	nic.EnableStatic(IPAddress=[ip], SubnetMask=[sub])
 	nic.SetGateways(DefaultIPGateway=[gateway])
 
+# List all network interface information
 def listNics(args):
 	for nic in nic_configs:
 		print(nic)
 
+# Assign ip using DHCP
 def autoAquireIP():
-	# Enable DHCP
 	nic = nic_configs[0]
 	nic.EnableDHCP()
 
+# TODO: Change to something better than ping!
+def reachable(ip):
+	response = os.system("ping -n 1 -c 1 " + ip)
+	output = ip + " Was unreachable "
+	if response == 0:
+		output = ip + " Acked "
+
+	return output
+
 # Get arguments
-parser = argparse.ArgumentParser(prog="pyip", description='Some usefull network commands')
+parser = argparse.ArgumentParser(prog="pyipchg", description='description: Change ip to static, or aquire from DHCP without dealin with windows GUI')
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
-#parser.add_argument('--listNics', action='listNics', help='list network interface cards information')
-parser.add_argument('IP', help='ip')
-parser.add_argument('SUBNET', help='subnetmask')
-parser.add_argument('GATWAY', help='gateway')
-
+parser.add_argument('-lnic', action='store_true', default=False,  help='list network interface cards information')
+parser.add_argument('-ip', help='ipv4 address')
+parser.add_argument('-sub', help='subnetmask, changes only if ip provided')
+parser.add_argument('-gat', help='Default Gateway, changes only if ip provided')
+parser.add_argument('-dhcp', action='store_true', default=False, help='Auto attain IP from DHCP')
+parser.add_argument('-reach', action='store', help='Check if ip is reachable')
 args = parser.parse_args()
-ipChange(args)
 
-#if args.listNics is not None:
-#	listNics(args)
+# Program flow dependant on optional args provided
+if args.lnic:
+	listNics(args)
 
+if args.ip is not None:
+	ipChange(args)
 
+elif args.dhcp is not None:
+	autoAquireIP()
 
+if len(sys.argv) == 1:
+	print("\tIP:     " + str(currentIP))
+	print("\tSubnet  " + str(currentSubnet))
+	print("\tGateway " + str(currentGateway))
+
+if args.reach is not None:
+		print(reachable(args.reach))
 
